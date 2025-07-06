aa0475e43e5cadd3bc3210d895ad7c9f85715b70
Alfonso <alfonsopiteo99@gmail.com>
2025-07-06 15:40:34 +0200

    Inizio progetto Arcadia - Unreal + C++

---
+++ Source/Arcadia/Arcadia.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Arcadia.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Arcadia, "Arcadia" );
+ 
